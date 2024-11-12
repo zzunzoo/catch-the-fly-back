55c8525c8f6b62cce3e9ffcdc4e6176e444b2f9e
이준혁 <leejunhyeok@z2h427.local>
2024-11-12 20:27:33 +0900

    1112

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'localhost:3000',
+      'https://port-next-catch-the-fly-front-m3ecukr559d1a7ca.sel4.cloudtype.app',
+    ],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 5000);
+}
+bootstrap();
